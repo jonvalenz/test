0a4737b192b97d44ce4177fec7dc801698fc2a39
jonvalenz <jon.valenzuela@mllrdev.com>
2022-01-25 11:28:38 +0800

    Prioritized ts rule for semicolons

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,8 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'linebreak-style': [1, 'windows'],
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
+    'semi': [0],
   },
 };
