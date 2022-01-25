37af0394978ab4c5ca862a34b18539355d8d0afd
jonvalenz <jon.valenzuela@mllrdev.com>
2022-01-25 11:31:09 +0800

    moved post to classes

--- src/models/post.ts
+++ src/models/post.ts
@@ -1,27 +1,5 @@
-class Post {
-  readonly id: number;
-
-  title: string;
-
-  content: string;
-
-  constructor(id?: number, title?: string, content?: string) {
-    this.id = id === undefined ? 0 : id;
-    this.title = title === undefined ? '' : title;
-    this.content = content === undefined ? '' : content;
-
-    return this;
-  }
-
-  static fromPost(post: Post): Post {
-    return new Post(post.id, post.title, post.content);
-  }
-}
-
-export interface IPost {
+export default interface IPost {
   readonly id: number;
   title: string;
   content: string;
 }
-
-export default Post;
